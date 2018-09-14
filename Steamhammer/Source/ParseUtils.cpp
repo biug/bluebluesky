ab47a4faf341e5f34f34d3df6d5511c628f70908
biug <zhangxunah@gmail.com>
2018-09-14 17:12:17 +0800

    判断是否有诱导水晶

--- Steamhammer/Source/ParseUtils.cpp
+++ Steamhammer/Source/ParseUtils.cpp
@@ -175,6 +175,7 @@ void ParseUtils::ParseConfigFile(const std::string & filename)
 		Config::Strategy::UsePlanRecognizer = GetBoolByRace("UsePlanRecognizer", strategy);
 		Config::Strategy::SurrenderWhenHopeIsLost = GetBoolByRace("SurrenderWhenHopeIsLost", strategy);
 		Config::Strategy::TrainingMode = GetBoolByRace("TrainingMode", strategy);
+		Config::Strategy::EnemyLurePylon = GetBoolByRace("EnemyLurePylon", strategy);
 
 		bool openingStrategyDecided = false;
 
